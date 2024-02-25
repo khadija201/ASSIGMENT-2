a2f5c61eaa3b9c711ad871ec47312a6a1184bee2
khadija201 <khadija2qubra@gmail.com>
2024-02-25 19:06:48 +0500

    assigment 17  file

---
+++ app.js
@@ -0,0 +1,106 @@
+// CHAPTERS
+// chp # 4  (1)
+var firstname ="khadija" ,secondname ="javed", qualification ="BS" 
+document.write("My name is ",firstname ," ", secondname, ". and Qualification is " ,qualification," ." )
+// (2)
+document.write( "<br><b>Rules for nameing js variables</b>"  ) 
+var num="number", num2=" $ ",let="letter", cast="sensetive" ,key="keyword"
+
+document.write( "<br>variable name can only contain ", num ,"and", num2,"For example:<b> $my - 1varsion </b>."
+,"<br>  variable must be begin with a ",let ," . For example:<b>  name , $veiw</b> .",
+"<br> variable name cast ",cast,".", "<br> variable name shouldnot be js ",key ,"."  )
+
+// chp # 5  (1 and 2)
+var num= 3+5
+console.log(num)
+var num2= 3-5
+console.log(num2)
+var num3= 3 % 5
+console.log(num3)
+var num4= 3*5
+console.log(num4)
+
+// 3
+var inc= 13;
+var dec=12 ;
+var add=5;
+console.log(  inc + inc++ + add++ - --dec + dec++ )
+// 4
+var tab= 4;
+var tab1= tab*1 ,tab2= tab*2,tab3= tab*3, tab4= tab*4, tab5= tab*5, tab6= tab*6,  tab7= tab*7,tab8= tab*8, 
+tab9= tab*9,tab10= tab*10;
+document.write( "<br>","Table of 4",  
+"<br>", tab, "*1=", tab1,
+"<br>", tab, "*2=", tab2,
+"<br>", tab, "*3=", tab3,
+"<br>", tab, "*4=", tab4,
+"<br>", tab, "*5=", tab5,
+"<br>", tab, "*6=", tab6,
+"<br>", tab, "*7=", tab7,
+"<br>", tab, "*8=", tab8,
+"<br>", tab, "*9=", tab9,
+"<br>", tab, "*10=", tab10 )
+
+// // chp# 6
+// // (1)
+ var a=2 , b=1;
+ var cal=--a - --b + ++b + b--;
+ console.log(cal);
+ var main= --a - --b + ++b;
+ console.log(main);
+// (2)
+
+// continue
+
+
+// home chapter 
+// (1)
+var Num =2+(2*6)
+console.log(Num)
+
+var Num1=(2+2)*6
+console.log(Num1)
+
+var Num2=(2+2)*(4+2);
+console.log(Num2)
+
+var Num3=((2+2)*4)+2;
+console.log(Num3)
+// 2
+var nam="2"+"2"
+console.log(nam)
+
+var massage=("hello " + " dolly")
+alert(massage)
+ 
+alert("33"+3);
+
+alert("Pakistan  Zindabad")
+
+alert(55)
+
+// 3
+var name= prompt("Enter first name")
+var name1= prompt("Conutry","china")
+var yourname =prompt("Enter your name") 
+
+var tab= +prompt("write any number")
+document.write( "<br>",  
+"<br>", tab, "*1=", tab1,
+"<br>", tab, "*2=", tab2,
+"<br>", tab, "*3=", tab3,
+"<br>", tab, "*4=", tab4,
+"<br>", tab, "*5=", tab5,
+"<br>", tab, "*6=", tab6,
+"<br>", tab, "*7=", tab7,
+"<br>", tab, "*8=", tab8,
+"<br>", tab, "*9=", tab9,
+"<br>", tab, "*10=", tab10 )
+
+
+
+
+
+
+
+
